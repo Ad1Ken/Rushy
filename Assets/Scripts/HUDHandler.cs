bfb94b861e8b4538e26cb95a0452253d320bbd32
Kenji <56013517+NekoArt@users.noreply.github.com>
2021-03-08 18:58:12 +0530

    Rushy 1.0

---
+++ Assets/Scripts/HUDHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUDHandler : MonoBehaviour
+{
+    public GameObject Maingame;
+    public GameObject MenuPanel;
+    public GameObject GameOverPanel;
+
+   
+
+
+
+
+
+    private void Start()
+    {
+        ActiveGameState(HUDstate.Menu);
+    }
+
+    public void ActiveGameState(HUDstate state)
+    {
+        if (state == HUDstate.Menu)
+        {
+            MenuPanel.SetActive(true);
+           
+            GameOverPanel.SetActive(false);
+            
+
+        }
+        
+        else if(state == HUDstate.Game)
+        {
+            
+            Maingame.SetActive(true);
+            MenuPanel.SetActive(false);
+        }
+        else if(state==HUDstate.GameOver)
+        {
+            GameOverPanel.SetActive(true);
+            Maingame.SetActive(false);
+           
+        }
+
+
+    }
+
+
+
+
+    public void OnClickPlay()
+    {
+        ActiveGameState(HUDstate.Game);
+
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickReturn()
+    {
+        ActiveGameState(HUDstate.Menu);
+        Clean();
+    }
+    
+    public void Clean()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Game");
+
+    }
+
+    
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
+    public Animator camAnim;
+
+    public Text healthDisplay;
+
+    public GameObject effect;
+    public GameObject EndPanel;
+
+   
+    void Update()
+    {
+        healthDisplay.text = health.ToString();
+        if (health <= 0)
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            EndPanel.SetActive(true);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y<maxHeight)
+        {
+            camAnim.SetTrigger("Shaky");
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y>minHeight)
+        {
+            camAnim.SetTrigger("Shaky");
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+        }
+    }
+
+    
+}
